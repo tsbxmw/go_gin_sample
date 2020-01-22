8b73296362f5950f8e803d8a676f94be30fb8886
mengwei <1050636648@qq.com>
2020-01-22 14:28:39 +0800

    change all other name to project

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	httpServer := http.HttpServer{}
 	config := common.ServiceConfigImpl{}
-	app, err := common.App("car_phone_v1", "wechat notification for car user", httpServer, config)
+	app, err := common.App("project", "sample for project of gin", httpServer, config)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
